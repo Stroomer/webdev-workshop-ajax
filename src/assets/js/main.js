8a4a3cd3b0d8afc626714a017c6aa80e8ba5dd46
Stroomer <lstroomer@glu.nl>
2022-09-08 12:54:23 +0200

    workshop complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -44,3 +44,46 @@ createComboboxFromArray( {name:'waardering',
 // De options worden gevuld vanuit een (extern) JSON-bestand.
 // De data uit dit JSON-bestand kan natuurlijk ook door een database worden verstrekt. 
 // ------------------------------------------------------------------------------
+
+function createComboboxFromJSON(params)
+{
+    const select = document.createElement('select');
+    const label  = document.createElement('label');
+    const parent = params.parent;
+    const options = params.options;
+
+    label.for       = params.label.for;
+    label.innerHTML = params.label.innerHTML;
+
+    select.name     = params.name;
+    select.id       = params.id;
+    select.addEventListener('change', function(event) {
+        console.log(event.target.value);
+    });
+
+    fetch('/src/assets/json/movies.json')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const options = data.options;
+        for(let i=0; i<options.length; i++) 
+        {
+            const option = document.createElement('option');
+            option.value = options[i];
+            option.innerText = options[i];
+            select.append(option);
+        }
+      })
+
+
+
+    parent.append(label);
+    parent.append(select);
+}
+
+createComboboxFromJSON( {name:'film',
+                         id:'film',
+                         parent:document.getElementById('dynamische_combo_json'),
+                         json:'movies.json',
+                         label:{for:'film', innerHTML:'Kies film:'}} );
