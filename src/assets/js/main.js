e79d752baa22a4168439b5f87ad28c96f4a382f1
Stroomer <lstroomer@glu.nl>
2022-09-08 10:00:19 +0200

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+// ------------------------------------------------------------------------------
+// Onderstaande functie is voor het (dynamisch) aanmaken van een Combobox.
+// De options worden gevuld vanuit een array die in de parameters is meegegeven.
+// ------------------------------------------------------------------------------
+
+
+
+// ------------------------------------------------------------------------------
+// Onderstaande functie is voor het (dynamisch) aanmaken van een Combobox.
+// De options worden gevuld vanuit een (extern) JSON-bestand.
+// De data uit dit JSON-bestand kan natuurlijk ook door een database worden verstrekt. 
+// ------------------------------------------------------------------------------
